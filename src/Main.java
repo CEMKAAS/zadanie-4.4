2489fc8de16e3f13e41ef3286b9bb8db941d3083
= <zaroslikob@mail.ru>
2022-09-12 14:29:21 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,33 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Person> queue1 = new LinkedList<>();
-        queue1.addAll(generateClients());
-        while (!queue1.isEmpty()) {
-            Person person = queue1.poll();
-            Person person1 = queue1.poll();
-            Person person2 = queue1.poll();
-            Person person3 = queue1.poll();
-            Person person4 = queue1.poll();
+        int counter = 0;
+        Queue<Person> queue = new LinkedList<>();
+        queue.addAll(generateClients());
+        while (!queue.isEmpty()) {
+            Person person = queue.poll();
+            Person person1 = queue.poll();
+            Person person2 = queue.poll();
+            Person person3 = queue.poll();
+            Person person4 = queue.poll();
             Person[] people = new Person[]{person, person1, person2, person3, person4};
-            System.out.println();
+            counter++;
+            System.out.println("--------" + counter + " Круг ---------------");
             for (Person personGeneral : people)
                 if (personGeneral != null && personGeneral.ticket()) {
                     System.out.println(personGeneral.getName() + " " + personGeneral.getFamily() + " На Атракционе");
-                    queue1.offer(personGeneral);
-
+                    queue.offer(personGeneral);
                 }
         }
     }
 
-
     public static List<Person> generateClients() {
-        List<Person> queue = new LinkedList<>();
-        queue.add(new Person("Семён", "Афанасьев", 2));
-        queue.add(new Person("Александр", "Филотов", 2));
-        queue.add(new Person("Танька", "Палкина", 3));
-        queue.add(new Person("Александр", "Кононов", 4));
-        queue.add(new Person("Сережа", "Михайлов", 3));
-        return queue;
+        List<Person> list = new LinkedList<>();
+        list.add(new Person("Олег", "Афанасьев", 2));
+        list.add(new Person("Александр", "Филотов", 2));
+        list.add(new Person("Танька", "Палкина", 3));
+        list.add(new Person("Александр", "Кононов", 4));
+        list.add(new Person("Сережа", "Михайлов", 3));
+        return list;
     }
 }
