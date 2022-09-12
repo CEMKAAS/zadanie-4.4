6456d0ae9ed4004eba34f4f50ddf5d593b3f7f93
= <zaroslikob@mail.ru>
2022-09-12 20:07:57 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,13 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        int counter = 0;
         Queue<Person> queue = new LinkedList<>();
         queue.addAll(generateClients());
         while (!queue.isEmpty()) {
             Person person = queue.poll();
-            Person person1 = queue.poll();
-            Person person2 = queue.poll();
-            Person person3 = queue.poll();
-            Person person4 = queue.poll();
-            Person[] people = new Person[]{person, person1, person2, person3, person4};
-            counter++;
-            System.out.println("--------" + counter + " Круг ---------------");
-            for (Person personGeneral : people)
-                if (personGeneral != null && personGeneral.ticket()) {
-                    System.out.println(personGeneral.getName() + " " + personGeneral.getFamily() + " На Атракционе");
-                    queue.offer(personGeneral);
+                if (person != null && person.ticket()) {
+                    System.out.println(person.getName() + " " + person.getFamily() + " На Атракционе, осталось билетов " + person.getTicket());
+                    queue.offer(person);
                 }
         }
     }
--- src/Person.java
+++ src/Person.java
@@ -25,5 +25,7 @@ public class Person {
         return true;
     }
 
-
+    public int getTicket() {
+        return ticket;
+    }
 }
